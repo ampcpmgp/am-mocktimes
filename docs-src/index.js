34c3428c6d87c181bdd407df963f1a7fc4ce4076
ampcpmgp <ampcpmgp@gmail.com>
2018-02-26 04:07:18 +0900

    gh-pages用ページをうつす、docs-src配下にうつす

--- docs-src/index.js
+++ docs-src/index.js
@@ -1,4 +1,4 @@
-import start from './main'
+import start from '../src/main'
 
 // TODO: gh-page用patternを作る(index.html, app.js ごと gh-pages directoryに移動しても良いかも)
 // そのパターンに紐づくhtmlファイルを作る -> docsに直で置く
@@ -10,4 +10,6 @@ import start from './main'
 // docs/ イメージ2
 // src/gh-page を作り、コンパイル用、コンパイルしない用の両方のファイルを置く
 // parcel -> docs/ && コンパイルしない用ファイルの移動
+//
+// TODO: required yaml
 start()
