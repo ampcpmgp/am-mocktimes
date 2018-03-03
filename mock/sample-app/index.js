dc54e656696674ff4822aa0e16a08f27aea6e1e4
ampcpmgp <ampcpmgp@gmail.com>
2018-03-03 21:07:05 +0900

    一旦動いたところまで

--- mock/sample-app/index.js
+++ mock/sample-app/index.js
@@ -1,13 +1,39 @@
+import sleep from '../../src/utils/sleep'
 import mock from '../../src/mock'
 
-// mock code
+const locations = {
+  A: ['Earth', 'Mars', 'Sun'],
+  B: ['Submaline City', 'Hanging Garden'],
+  Z: ['Capsule Corporation', 'Yahhoi', 'Cell Game']
+}
+
+window.setLocation = async (location, animation) => {
+  const message = `This is ${location}`
+  window.catSaysLocation.src = `https://cataas.com/cat/says/${message}`
+  window.catSaysLocation.alt = `Image loading...`
+  window.catSaysLocation.onerror = () => {
+    window.catSaysLocation.alt = `Image not found. - ${message}`
+  }
+  if (animation) {
+    window.catSaysLocation.onload = () => {
+      window.catSaysLocation.style.animation = ``
+      window.catSaysLocation.style.animation = `spin 0.8s ease`
+    }
+  }
+}
+
+const setPlan = async (planName) => {
+  await sleep(1400)
+  window.planName.textContent = planName
+  window.locationBox.innerHTML =
+    locations[planName].reduce((html, location) =>
+    `${html}<div onclick='setLocation("${location}")'>${location}</div>`,
+    '')
+}
+
 mock({
-  setPlan: async (planFile) => {
-    console.warn(planFile)
-  },
-  setLocation (location, animationName) {
-    console.warn(location, animationName)
-  },
+  setPlan,
+  setLocation: window.setLocation,
   goLocation () {
     console.warn('go location')
   },
@@ -15,5 +41,3 @@ mock({
     console.warn('click', selector)
   }
 })
-
-// prodcut code
