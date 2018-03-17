b8cf4500fbd4a25e663f092df412dc5e063c7ca5
ampcpmgp <ampcpmgp@gmail.com>
2018-03-18 03:23:41 +0900

    move babel-polyfill

--- bin/mock-js.js
+++ bin/mock-js.js
@@ -5,7 +5,8 @@ module.exports = (outputDir, configFile, appFilePath, scriptSrc) => {
   const configFilePath = path.relative(outputDir, configFile)
   const productionFilePath = path.relative(outputDir, path.join(appFilePath, scriptSrc))
 
-  return `import '${normalize(configFilePath)}'
+  return `import 'babel-polyfill'
+import '${normalize(configFilePath)}'
 import '${normalize(productionFilePath)}'
 
 if (module.hot) {
