0f64d26d7907bc5d962ec7d4852ecdbbcbd5a3a9
ampcpmgp <ampcpmgp@gmail.com>
2018-03-28 00:49:08 +0900

    log改善

--- bin/output-template-log.js
+++ bin/output-template-log.js
@@ -1,10 +1,11 @@
 module.exports = () => {
-  console.log(`以下ファイルを生成しました
+  console.log(`
+以下のファイルを生成しました
 
 # モック
 mock/
-  pattern.yml # モックパターン設定用
-  config.js # アプリケーションモック設定用
+  pattern.yml # モック一覧画面/設定用
+  config.js # アプリケーションモック/設定用
 
 # アプリケーション本体
 src/
