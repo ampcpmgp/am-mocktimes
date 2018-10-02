9e07fa43e4abdbd744bb2a5dc96b8e3890e0fa8d
Akira Hayatake <akira.hayatake@optim.co.jp>
2018-10-02 16:37:33 +0900

    テストが通らない時があったのを修正

--- bin/test/index.js
+++ bin/test/index.js
@@ -1,48 +1,55 @@
-const { describe, it, before } = require('kocha')
+const { describe, it } = require('kocha')
 const waitOn = require('wait-on')
 const fs = require('fs-extra')
 const assert = require('assert')
 const util = require('util')
 const { exec } = require('child_process')
 
-const execPromise = util.promisify(exec)
+const execAsync = util.promisify(exec)
 
 describe('watch & screenshot', () => {
-  before(async () => {
-    execPromise(
-      'node ./bin watch -a mock/index.html --ss mock/sample-app/index.html --no-open'
-    )
-
-    return new Promise((resolve, reject) => {
+  it('画像フォルダ内が空っぽになり、新規画像が撮られていること', async () => {
+    const waitServer = new Promise((resolve, reject) => {
       waitOn(
         {
-          resources: ['http://localhost:1234/.am-mocktimes/pattern.html']
+          resources: ['http://localhost:1234/.am-mocktimes/pattern.html'],
+          timeout: 2000
         },
         err => {
-          if (err) reject(err)
-          else resolve()
+          if (err) {
+            assert(
+              0,
+              'サーバーを起動(npm start)した状態で、テストを回してください。'
+            )
+            reject(err)
+          } else resolve()
         }
       )
     })
-  })
 
-  it('画像フォルダ内が空っぽになり、新規画像が撮られていること', async () => {
-    const job = execPromise(
+    await waitServer
+
+    const job = execAsync(
       'node ./bin screenshot --url http://localhost:1234/.am-mocktimes/pattern.html'
     )
+    const imgDir = './am-mocktimes-img'
     let isRemovedImgFiles = false
     let isCapturedImgFiles = false
 
     // 画像削除の確認
     const checkIfImgFilesRemoved = () => {
-      const intervalId = setInterval(() => {
-        const files = fs.readdirSync('./am-mocktimes-img')
-        const isEmpty = files.length === 0
+      const intervalId = setInterval(async () => {
+        try {
+          const files = await fs.readdir(imgDir)
+          const isEmpty = files.length === 0
 
-        if (isEmpty) {
-          isRemovedImgFiles = true
-          clearInterval(intervalId)
-          checkIfImgFilesExists()
+          if (isEmpty) {
+            isRemovedImgFiles = true
+            clearInterval(intervalId)
+            checkIfImgFilesExists()
+          }
+        } catch (error) {
+          // do nothing
         }
       }, 100)
     }
@@ -50,7 +57,7 @@ describe('watch & screenshot', () => {
     // 新規画像が撮られていることの確認
     const checkIfImgFilesExists = () => {
       const intervalId = setInterval(() => {
-        const files = fs.readdirSync('./am-mocktimes-img')
+        const files = fs.readdirSync(imgDir)
         const isEmpty = files.length === 0
 
         if (!isEmpty) {
