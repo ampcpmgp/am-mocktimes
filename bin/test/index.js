fa1ed2f8a6d64c534e1475246111913e75783169
ampcpmgp <ampcpmgp@gmail.com>
2018-10-04 05:01:37 +0900

    スクリーンショットのテストコードリファクタ (#55)

--- bin/test/index.js
+++ bin/test/index.js
@@ -1,14 +1,13 @@
-const { describe, it } = require('kocha')
+const { describe, it, before } = require('kocha')
 const waitOn = require('wait-on')
 const fs = require('fs-extra')
-const assert = require('assert')
 const util = require('util')
 const { exec } = require('child_process')
 
 const execAsync = util.promisify(exec)
 
 describe('screenshot', () => {
-  it('画像フォルダ内が空っぽになり、新規画像が撮られていること', async () => {
+  before(async () => {
     const waitServer = new Promise((resolve, reject) => {
       waitOn(
         {
@@ -17,56 +16,85 @@ describe('screenshot', () => {
         },
         err => {
           if (err) {
-            assert(
-              0,
-              'サーバーを起動(npm start)した状態で、テストを回してください。'
-            )
             reject(err)
           } else resolve()
         }
       )
     })
 
-    await waitServer
+    try {
+      await waitServer
+    } catch (error) {
+      throw new Error(
+        'サーバーを起動(npm start)した状態で、テストを回してください。'
+      )
+    }
+  })
 
+  it('画像フォルダ内が空っぽになり、新規画像が撮られていること', async () => {
     const job = execAsync(
       'node ./bin screenshot --url http://localhost:1234/.am-mocktimes/pattern.html'
     )
     const imgDir = './am-mocktimes-img'
-    let isRemovedImgFiles = false
-    let isCapturedImgFiles = false
 
     // 画像削除の確認
-    const checkIfImgFilesRemoved = () => {
+    const checkIfImgFilesRemoved = new Promise((resolve, reject) => {
+      const timeout = 5000
+      const now = Date.now()
+
       const intervalId = setInterval(async () => {
         const files = await fs.readdir(imgDir)
         const isEmpty = files.length === 0
 
         if (isEmpty) {
-          isRemovedImgFiles = true
           clearInterval(intervalId)
-          checkIfImgFilesExists()
+          resolve()
+        }
+
+        const elapsedTime = Date.now() - now
+        if (elapsedTime > timeout) {
+          reject(new Error('timeout'))
         }
       }, 100)
+    })
+
+    try {
+      await checkIfImgFilesRemoved
+    } catch (error) {
+      throw new Error('画像ファイルの削除が確認できません。')
     }
 
     // 新規画像が撮られていることの確認
-    const checkIfImgFilesExists = () => {
-      const intervalId = setInterval(() => {
-        const files = fs.readdirSync(imgDir)
+    const checkIfImgFilesExists = new Promise((resolve, reject) => {
+      const timeout = 5000
+      const now = Date.now()
+
+      const intervalId = setInterval(async () => {
+        const files = await fs.readdir(imgDir)
         const isEmpty = files.length === 0
 
         if (!isEmpty) {
-          isCapturedImgFiles = true
           clearInterval(intervalId)
+          resolve()
+        }
+
+        const elapsedTime = Date.now() - now
+        if (elapsedTime > timeout) {
+          reject(new Error('timeout'))
         }
       }, 100)
+    })
+
+    try {
+      await checkIfImgFilesExists
+    } catch (error) {
+      throw new Error('新規画像が撮られていることを確認できません。')
     }
 
-    checkIfImgFilesRemoved()
-
-    await job
-
-    assert(isRemovedImgFiles && isCapturedImgFiles)
+    try {
+      await job
+    } catch (error) {
+      throw new Error('job error')
+    }
   })
 })
