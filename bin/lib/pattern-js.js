90cd51ebadac8026c2fa9d8ea8b2b5256c626585
ampcpmgp <ampcpmgp@gmail.com>
2018-12-04 20:27:55 +0900

    Fix pattern js relative path (#58)
    * Update pattern-js.js
    
    * Update util.js
    
    * 1.7.4

--- bin/lib/pattern-js.js
+++ bin/lib/pattern-js.js
@@ -1,6 +1,8 @@
+const normalize = require('normalize-path')
+
 module.exports = patternFile => {
   return `import {generateList} from 'am-mocktimes/src/main'
-import pattern from '../${patternFile}'
+import pattern from '${normalize(patternFile)}'
 
 generateList(pattern)
 `
