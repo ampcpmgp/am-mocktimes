b418daa8d6d393a81da6566f9e17153f73b33d11
Akira Hayatake <akira.hayatake@optim.co.jp>
2018-12-05 16:35:41 +0900

    テスト追加

--- bin/test/watch.js
+++ bin/test/watch.js
@@ -6,20 +6,21 @@ const assert = require('assert')
 const { exec } = require('child_process')
 
 const execAsync = util.promisify(exec)
+const dir = '.test'
 
 describe('watch', () => {
   it('パターンファイルのみビルドされること', async () => {
-    await fs.remove('.am-mocktimes')
+    await fs.remove(dir)
     execAsync(
-      `node ./bin/index.js watch --only-pattern --app mock/index.html --open false`
+      `node ./bin/index.js watch --only-pattern --app mock/index.html --open false --out-dir ${dir}`
     )
 
     await waitOn({
-      resources: ['.am-mocktimes/pattern.html'],
+      resources: [`${dir}/pattern.html`],
       timeout: 4000
     })
 
-    const existsMockHtml = await fs.pathExists('.am-mocktimes/mock.html')
+    const existsMockHtml = await fs.pathExists(`${dir}/mock.html`)
 
     assert(!existsMockHtml)
   })
